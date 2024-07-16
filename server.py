a44f039382dc9c781bba8745d1f291b5f09f0ea1
andrea <andrea.marchisi@gmail.com>
2024-07-16 17:02:54 +0200

    Add logging for debugging

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 
 @app.route('/gpt-endpoint', methods=['GET'])
 def gpt_endpoint():
+    app.logger.info("Endpoint /gpt-endpoint was called")
     return jsonify({"message": "Hello, Homey GPT!"})
 
 if __name__ == '__main__':
