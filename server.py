d1e2d112525afcd14519d019fa874f0f198294f2
andrea <andrea.marchisi@gmail.com>
2024-07-16 17:14:22 +0200

    Add logging for endpoint call

--- server.py
+++ server.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 
 @app.route('/gpt-endpoint', methods=['GET'])
 def gpt_endpoint():
+    app.logger.info("Endpoint /gpt-endpoint was called")
     return jsonify({"message": "Hello, Homey GPT!"})
 
 if __name__ == '__main__':
+    app.logger.info("Starting Flask application")
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
